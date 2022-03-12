babda3c4e2c6f2c6c13bb5d2a6465cb4d2ed606e
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-12 17:09:48 +0800

    wip - the lexer

---
+++ token/token.go
@@ -0,0 +1,29 @@
+package token
+
+type TokenType string
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+	// Identifiers + literals
+	IDENT = "IDENT"
+	INT   = "INT"
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
