c669e9c5dda6db9efaf2874cdc63fdb061049738
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-12 18:27:13 +0800

    wip - support operators ==, !=

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -86,6 +86,14 @@ func (l *Lexer) readChar() {
 	l.readPosition += 1
 }
 
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	} else {
+		return l.input[l.readPosition]
+	}
+}
+
 func (l *Lexer) readIdentifier() string {
 	position := l.position
 	for isLetter(l.ch) {
