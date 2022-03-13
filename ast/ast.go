ff93718753877414e8200c8934b8f9aae692e113
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-13 17:07:21 +0800

    wip - parse boolean expression

--- ast/ast.go
+++ ast/ast.go
@@ -95,6 +95,15 @@ func (es *ExpressionStatement) String() string {
 	return ""
 }
 
+type Boolean struct {
+	Token token.Token
+	Value bool
+}
+
+func (b *Boolean) expressionNode()      {}
+func (b *Boolean) TokenLiteral() string { return b.Token.Literal }
+func (b *Boolean) String() string       { return b.Token.Literal }
+
 type Identifier struct {
 	Token token.Token
 	Value string
