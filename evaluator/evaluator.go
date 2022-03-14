6c55b088cbf8bf65eda8d052879e08e9eaa303f1
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-14 11:33:24 +0800

    done - evaluate boolean expression

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,11 @@ import (
 	"monkey/object"
 )
 
+var (
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	// Statements
@@ -16,7 +21,7 @@ func Eval(node ast.Node) object.Object {
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
-		return &object.Boolean{Value: node.Value}
+		return nativeBoolToBooleanObject(node.Value)
 	}
 	return nil
 }
@@ -27,4 +32,11 @@ func evalStatements(stmts []ast.Statement) object.Object {
 		result = Eval(statement)
 	}
 	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
