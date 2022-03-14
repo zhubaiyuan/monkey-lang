4ff501a11a02f0968b7cd94262dd7b06ec29f8eb
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-15 02:19:03 +0800

    wip - evaluate string literal

--- object/object.go
+++ object/object.go
@@ -14,6 +14,7 @@ const (
 	ERROR_OBJ        = "ERROR"
 	INTEGER_OBJ      = "INTEGER"
 	BOOLEAN_OBJ      = "BOOLEAN"
+	STRING_OBJ       = "STRING"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	FUNCTION_OBJ     = "FUNCTION"
 )
@@ -77,3 +78,10 @@ func (f *Function) Inspect() string {
 	out.WriteString("\n}")
 	return out.String()
 }
+
+type String struct {
+	Value string
+}
+
+func (s *String) Type() ObjectType { return STRING_OBJ }
+func (s *String) Inspect() string  { return s.Value }
