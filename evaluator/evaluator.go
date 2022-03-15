4f61de1880ada6067bb21de1233dfd0e182ff142
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-15 12:32:34 +0800

    wip - define len identifier

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -217,11 +217,13 @@ func evalIfExpression(ie *ast.IfExpression, env *object.Environment) object.Obje
 }
 
 func evalIdentifier(node *ast.Identifier, env *object.Environment) object.Object {
-	val, ok := env.Get(node.Value)
-	if !ok {
-		return newError("identifier not found: " + node.Value)
+	if val, ok := env.Get(node.Value); ok {
+		return val
 	}
-	return val
+	if builtin, ok := builtins[node.Value]; ok {
+		return builtin
+	}
+	return newError("identifier not found: " + node.Value)
 }
 
 func isTruthy(obj object.Object) bool {
