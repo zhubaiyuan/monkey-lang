c0141a14a50124c896f16e58aa2b3f86c0be9518
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-16 17:05:36 +0800

    done - hash literal

--- ast/modify.go
+++ ast/modify.go
@@ -41,6 +41,14 @@ func Modify(node Node, modifier ModifierFunc) Node {
 		for i := range node.Elements {
 			node.Elements[i], _ = Modify(node.Elements[i], modifier).(Expression)
 		}
+	case *HashLiteral:
+		newPairs := make(map[Expression]Expression)
+		for key, val := range node.Pairs {
+			newKey, _ := Modify(key, modifier).(Expression)
+			newVal, _ := Modify(val, modifier).(Expression)
+			newPairs[newKey] = newVal
+		}
+		node.Pairs = newPairs
 	}
 	return modifier(node)
 }
--- ast/modify_test.go
+++ ast/modify_test.go
@@ -121,4 +121,21 @@ func TestModify(t *testing.T) {
 			t.Errorf("not equal. got=%#v, want=%#v", modified, tt.expected)
 		}
 	}
+	hashLiteral := &HashLiteral{
+		Pairs: map[Expression]Expression{
+			one(): one(),
+			one(): one(),
+		},
+	}
+	Modify(hashLiteral, turnOneIntoTwo)
+	for key, val := range hashLiteral.Pairs {
+		key, _ := key.(*IntegerLiteral)
+		if key.Value != 2 {
+			t.Errorf("value is not %d, got=%d", 2, key.Value)
+		}
+		val, _ := val.(*IntegerLiteral)
+		if val.Value != 2 {
+			t.Errorf("value is not %d, got=%d", 2, val.Value)
+		}
+	}
 }
