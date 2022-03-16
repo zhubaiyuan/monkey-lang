7832cb470d22333031ee03e35660d899a6f9740f
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-16 17:18:58 +0800

    wip - replacing unquote calls

--- evaluator/quote_unquote.go
+++ evaluator/quote_unquote.go
@@ -5,6 +5,31 @@ import (
 	"monkey/object"
 )
 
-func quote(node ast.Node, env *object.Environment) object.Object {
+func quote(node ast.Node) object.Object {
+	node = evalUnquoteCalls(node)
 	return &object.Quote{Node: node}
+}
+
+func evalUnquoteCalls(quoted ast.Node) ast.Node {
+	return ast.Modify(quoted, func(node ast.Node) ast.Node {
+		if !isUnquoteCall(node) {
+			return node
+		}
+		call, ok := node.(*ast.CallExpression)
+		if !ok {
+			return node
+		}
+		if len(call.Arguments) != 1 {
+			return node
+		}
+		return node
+	})
+}
+
+func isUnquoteCall(node ast.Node) bool {
+	callExpression, ok := node.(*ast.CallExpression)
+	if !ok {
+		return false
+	}
+	return callExpression.Function.TokenLiteral() == "unquote"
 }
