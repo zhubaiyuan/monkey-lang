c10b360c9ef9f97bfd0834a8f68936b1dfcaa0f6
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-16 17:23:27 +0800

    wip - change the signature of quote and pass env on to evalUnquoteCalls

--- evaluator/quote_unquote.go
+++ evaluator/quote_unquote.go
@@ -5,12 +5,12 @@ import (
 	"monkey/object"
 )
 
-func quote(node ast.Node) object.Object {
-	node = evalUnquoteCalls(node)
+func quote(node ast.Node, env *object.Environment) object.Object {
+	node = evalUnquoteCalls(node, env)
 	return &object.Quote{Node: node}
 }
 
-func evalUnquoteCalls(quoted ast.Node) ast.Node {
+func evalUnquoteCalls(quoted ast.Node, env *object.Environment) ast.Node {
 	return ast.Modify(quoted, func(node ast.Node) ast.Node {
 		if !isUnquoteCall(node) {
 			return node
@@ -22,7 +22,7 @@ func evalUnquoteCalls(quoted ast.Node) ast.Node {
 		if len(call.Arguments) != 1 {
 			return node
 		}
-		return node
+		return Eval(call.Arguments[0], env)
 	})
 }
 
