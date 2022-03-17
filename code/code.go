870955118dcd4da0f3491cbb949112008fa56e31
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-17 21:41:28 +0800

    done - extend the switch statement in the Instructions.fmtInstruction method to handle opcodes with no operands

--- code/code.go
+++ code/code.go
@@ -30,6 +30,8 @@ func (ins Instructions) fmtInstruction(def *Definition, operands []int) string {
 		return fmt.Sprintf("ERROR: operand len %d does not match defined %d\n", len(operands), operandCount)
 	}
 	switch operandCount {
+	case 0:
+		return def.Name
 	case 1:
 		return fmt.Sprintf("%s %d", def.Name, operands[0])
 	}
