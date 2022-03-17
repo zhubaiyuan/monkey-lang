f07074ac479051a3ddab19e1ff6e09a7dde7992a
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-17 20:17:22 +0800

    wip - implement a ReadOperands function that reverses everything Make did

--- code/code_test.go
+++ code/code_test.go
@@ -41,3 +41,29 @@ func TestInstructionsString(t *testing.T) {
 		t.Errorf("instructions wrongly formatted.\nwant=%q\ngot=%q", expected, concatted.String())
 	}
 }
+
+func TestReadOperands(t *testing.T) {
+	tests := []struct {
+		op        Opcode
+		operands  []int
+		bytesRead int
+	}{
+		{OpConstant, []int{65535}, 2},
+	}
+	for _, tt := range tests {
+		instruction := Make(tt.op, tt.operands...)
+		def, err := Lookup(byte(tt.op))
+		if err != nil {
+			t.Fatalf("definition not found: %q\n", err)
+		}
+		operandsRead, n := ReadOperands(def, instruction[1:])
+		if n != tt.bytesRead {
+			t.Fatalf("n wrong. want=%d, got=%d", tt.bytesRead, n)
+		}
+		for i, want := range tt.operands {
+			if operandsRead[i] != want {
+				t.Errorf("operand wrong. want=%d, got=%d", want, operandsRead[i])
+			}
+		}
+	}
+}
