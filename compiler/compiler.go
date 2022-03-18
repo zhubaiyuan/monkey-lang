7ae3c5262c4b9dfcad73267cd8a9924807bd1fbc
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-18 20:15:55 +0800

    wip - add a new let statement case branch to compile()

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -137,6 +137,11 @@ func (c *Compiler) Compile(node ast.Node) error {
 				return err
 			}
 		}
+	case *ast.LetStatement:
+		err := c.Compile(node.Value)
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }
