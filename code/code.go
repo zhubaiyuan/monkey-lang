e2bbba92550fd298647889e292917899e2bfe924
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-19 01:04:58 +0800

    wip - support hash in the compiler

--- code/code.go
+++ code/code.go
@@ -60,6 +60,7 @@ const (
 	OpGetGlobal
 	OpSetGlobal
 	OpArray
+	OpHash
 )
 
 type Definition struct {
@@ -87,6 +88,7 @@ var definitions = map[Opcode]*Definition{
 	OpGetGlobal:     {"OpGetGlobal", []int{2}},
 	OpSetGlobal:     {"OpSetGlobal", []int{2}},
 	OpArray:         {"OpArray", []int{2}},
+	OpHash:          {"OpHash", []int{2}},
 }
 
 func Lookup(op byte) (*Definition, error) {
