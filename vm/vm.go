7ee4b40b90ff686cfa6d02812af851d68093e3ae
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-19 00:56:22 +0800

    done - support array in the vm

--- vm/vm.go
+++ vm/vm.go
@@ -119,6 +119,15 @@ func (vm *VM) Run() error {
 			if err != nil {
 				return err
 			}
+		case code.OpArray:
+			numElements := int(code.ReadUint16(vm.instructions[ip+1:]))
+			ip += 2
+			array := vm.buildArray(vm.sp-numElements, vm.sp)
+			vm.sp = vm.sp - numElements
+			err := vm.push(array)
+			if err != nil {
+				return err
+			}
 		}
 	}
 	return nil
@@ -234,6 +243,14 @@ func (vm *VM) executeBinaryStringOperation(op code.Opcode, left, right object.Ob
 	leftValue := left.(*object.String).Value
 	rightValue := right.(*object.String).Value
 	return vm.push(&object.String{Value: leftValue + rightValue})
+}
+
+func (vm *VM) buildArray(startIndex, endIndex int) object.Object {
+	elements := make([]object.Object, endIndex-startIndex)
+	for i := startIndex; i < endIndex; i++ {
+		elements[i-startIndex] = vm.stack[i]
+	}
+	return &object.Array{Elements: elements}
 }
 
 func nativeBoolToBooleanObject(input bool) *object.Boolean {
