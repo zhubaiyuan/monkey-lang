91071a8bf86c014104e68ed70062a6b25158b8fc
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-19 18:51:39 +0800

    done - turn the body of a function into a series of instructions

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -210,6 +210,12 @@ func (c *Compiler) Compile(node ast.Node) error {
 		instructions := c.leaveScope()
 		compiledFn := &object.CompiledFunction{Instructions: instructions}
 		c.emit(code.OpConstant, c.addConstant(compiledFn))
+	case *ast.ReturnStatement:
+		err := c.Compile(node.ReturnValue)
+		if err != nil {
+			return err
+		}
+		c.emit(code.OpReturnValue)
 	}
 	return nil
 }
