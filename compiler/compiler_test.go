d56471ea46bfe17623a4e81cb341214a1ed20fc8
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-19 18:21:51 +0800

    wip - compiling function literals

--- compiler/compiler_test.go
+++ compiler/compiler_test.go
@@ -271,7 +271,29 @@ func TestGlobalLetStatements(t *testing.T) {
 			},
 		},
 	}
+	runCompilerTests(t, tests)
+}
 
+func TestFunctions(t *testing.T) {
+	tests := []compilerTestCase{
+		{
+			input: `fn() { return 5 + 10 }`,
+			expectedConstants: []interface{}{
+				5,
+				10,
+				[]code.Instructions{
+					code.Make(code.OpConstant, 0),
+					code.Make(code.OpConstant, 1),
+					code.Make(code.OpAdd),
+					code.Make(code.OpReturnValue),
+				},
+			},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpConstant, 2),
+				code.Make(code.OpPop),
+			},
+		},
+	}
 	runCompilerTests(t, tests)
 }
 
@@ -483,6 +505,15 @@ func testConstants(t *testing.T, expected []interface{}, actual []object.Object)
 			err := testIntegerObject(int64(constant), actual[i])
 			if err != nil {
 				return fmt.Errorf("constant %d - testIntegerObject failed: %s", i, err)
+			}
+		case []code.Instructions:
+			fn, ok := actual[i].(*object.CompiledFunction)
+			if !ok {
+				return fmt.Errorf("constant %d - not a function: %T", i, actual[i])
+			}
+			err := testInstructions(constant, fn.Instructions)
+			if err != nil {
+				return fmt.Errorf("constant %d - testInstructions failed: %s", i, err)
 			}
 		}
 	}
