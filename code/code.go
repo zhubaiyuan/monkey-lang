46d64b6492d4a06f8c673aa21560de7270f7e62b
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-19 18:13:17 +0800

    done - opcodes to execute functions

--- code/code.go
+++ code/code.go
@@ -62,6 +62,9 @@ const (
 	OpArray
 	OpHash
 	OpIndex
+	OpCall
+	OpReturnValue
+	OpReturn
 )
 
 type Definition struct {
@@ -91,6 +94,9 @@ var definitions = map[Opcode]*Definition{
 	OpArray:         {"OpArray", []int{2}},
 	OpHash:          {"OpHash", []int{2}},
 	OpIndex:         {"OpIndex", []int{}},
+	OpCall:          {"OpCall", []int{}},
+	OpReturnValue:   {"OpReturnValue", []int{}},
+	OpReturn:        {"OpReturn", []int{}},
 }
 
 func Lookup(op byte) (*Definition, error) {
