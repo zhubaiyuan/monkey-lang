e5e1fe590576dd5a1724d33bbd76e4a71ae97fc2
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-20 00:08:09 +0800

    wip - resolve the given symbol

--- compiler/symbol_table.go
+++ compiler/symbol_table.go
@@ -44,5 +44,9 @@ func (s *SymbolTable) Define(name string) Symbol {
 
 func (s *SymbolTable) Resolve(name string) (Symbol, bool) {
 	obj, ok := s.store[name]
+	if !ok && s.Outer != nil {
+		obj, ok = s.Outer.Resolve(name)
+		return obj, ok
+	}
 	return obj, ok
 }
