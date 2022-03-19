5a1a1918a54a17b562dfa747c5f9d5d595e99bbf
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-19 20:25:31 +0800

    wip - TestFunctionsWithoutReturnValue

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -197,6 +197,28 @@ func TestFunctionsWithReturnStatement(t *testing.T) {
 	runVmTests(t, tests)
 }
 
+func TestFunctionsWithoutReturnValue(t *testing.T) {
+	tests := []vmTestCase{
+		{
+			input: `
+		let noReturn = fn() { };
+		noReturn();
+		`,
+			expected: Null,
+		},
+		{
+			input: `
+		let noReturn = fn() { };
+		let noReturnTwo = fn() { noReturn(); };
+		noReturn();
+		noReturnTwo();
+		`,
+			expected: Null,
+		},
+	}
+	runVmTests(t, tests)
+}
+
 type vmTestCase struct {
 	input    string
 	expected interface{}
