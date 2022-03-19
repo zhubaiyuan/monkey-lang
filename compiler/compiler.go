e6718e66e7f4c0f661e93522322b41642d7844b9
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-20 00:29:20 +0800

    done - compiling with scopes

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -164,7 +164,11 @@ func (c *Compiler) Compile(node ast.Node) error {
 		if !ok {
 			return fmt.Errorf("undefined variable %s", node.Value)
 		}
-		c.emit(code.OpGetGlobal, symbol.Index)
+		if symbol.Scope == GlobalScope {
+			c.emit(code.OpGetGlobal, symbol.Index)
+		} else {
+			c.emit(code.OpGetLocal, symbol.Index)
+		}
 	case *ast.StringLiteral:
 		str := &object.String{Value: node.Value}
 		c.emit(code.OpConstant, c.addConstant(str))
