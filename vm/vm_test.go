6d6383e629d52298b87c6406ef90f5c7abe13ba9
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-21 00:47:34 +0800

    done - extend test case

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -534,6 +534,22 @@ func TestRecursiveFunctions(t *testing.T) {
 		`,
 			expected: 0,
 		},
+		{
+			input: `
+		let countDown = fn(x) {
+			if (x == 0) {
+				return 0;
+			} else {
+				countDown(x - 1);
+			}
+		};
+		let wrapper = fn() {
+			countDown(1);
+		};
+		wrapper();
+		`,
+			expected: 0,
+		},
 	}
 	runVmTests(t, tests)
 }
