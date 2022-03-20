42c56084b392418ddf6bc57a4dc508e31a6a4886
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-20 23:41:55 +0800

    wip - compiling and resolving free variables

--- code/code.go
+++ code/code.go
@@ -71,6 +71,7 @@ const (
 	OpSetLocal
 	OpGetBuiltin
 	OpClosure
+	OpGetFree
 )
 
 type Definition struct {
@@ -107,6 +108,7 @@ var definitions = map[Opcode]*Definition{
 	OpSetLocal:      {"OpSetLocal", []int{1}},
 	OpGetBuiltin:    {"OpGetBuiltin", []int{1}},
 	OpClosure:       {"OpClosure", []int{2, 1}},
+	OpGetFree:       {"OpGetFree", []int{1}},
 }
 
 func Lookup(op byte) (*Definition, error) {
