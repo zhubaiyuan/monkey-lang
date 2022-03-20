681e2482753f529c77e39ac74b85de0c87489104
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-20 16:19:22 +0800

    done  - fix calling a function with the wrong number of arguments in the vm; done - support functions and function calls in the repl

--- object/object.go
+++ object/object.go
@@ -200,8 +200,9 @@ func (m *Macro) Inspect() string {
 }
 
 type CompiledFunction struct {
-	Instructions code.Instructions
-	NumLocals    int
+	Instructions  code.Instructions
+	NumLocals     int
+	NumParameters int
 }
 
 func (cf *CompiledFunction) Type() ObjectType { return COMPILED_FUNCTION_OBJ }
--- vm/vm.go
+++ vm/vm.go
@@ -388,6 +388,9 @@ func (vm *VM) callFunction(numArgs int) error {
 	if !ok {
 		return fmt.Errorf("calling non-function")
 	}
+	if numArgs != fn.NumParameters {
+		return fmt.Errorf("wrong number of arguments: want=%d, got=%d", fn.NumParameters, numArgs)
+	}
 	frame := NewFrame(fn, vm.sp-numArgs)
 	vm.pushFrame(frame)
 	vm.sp = frame.basePointer + fn.NumLocals
