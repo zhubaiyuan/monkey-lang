0eaa9c546f3945647c313a6407b99379e935bd2d
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-21 00:05:22 +0800

    done - a fully-functioning symbol table knows about free variables

--- compiler/symbol_table.go
+++ compiler/symbol_table.go
@@ -50,7 +50,14 @@ func (s *SymbolTable) Resolve(name string) (Symbol, bool) {
 	obj, ok := s.store[name]
 	if !ok && s.Outer != nil {
 		obj, ok = s.Outer.Resolve(name)
-		return obj, ok
+		if !ok {
+			return obj, ok
+		}
+		if obj.Scope == GlobalScope || obj.Scope == BuiltinScope {
+			return obj, ok
+		}
+		free := s.defineFree(obj)
+		return free, true
 	}
 	return obj, ok
 }
@@ -58,5 +65,13 @@ func (s *SymbolTable) Resolve(name string) (Symbol, bool) {
 func (s *SymbolTable) DefineBuiltin(index int, name string) Symbol {
 	symbol := Symbol{Name: name, Index: index, Scope: BuiltinScope}
 	s.store[name] = symbol
+	return symbol
+}
+
+func (s *SymbolTable) defineFree(original Symbol) Symbol {
+	s.FreeSymbols = append(s.FreeSymbols, original)
+	symbol := Symbol{Name: original.Name, Index: len(s.FreeSymbols) - 1}
+	symbol.Scope = FreeScope
+	s.store[original.Name] = symbol
 	return symbol
 }
