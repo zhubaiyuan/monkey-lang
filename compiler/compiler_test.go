6426e23f9b6222380d4afe3f56fa2b7782137462
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-20 14:45:24 +0800

    wip - give the OpCall opcode an operand that holds the number of arguments of the call

--- compiler/compiler_test.go
+++ compiler/compiler_test.go
@@ -568,9 +568,9 @@ func TestFunctionCalls(t *testing.T) {
 				},
 			},
 			expectedInstructions: []code.Instructions{
-				// The compiled function code.Make(code.OpCall),
+				// The compiled function
 				code.Make(code.OpConstant, 1),
-				code.Make(code.OpCall),
+				code.Make(code.OpCall, 0),
 				code.Make(code.OpPop),
 			},
 		},
@@ -592,7 +592,7 @@ func TestFunctionCalls(t *testing.T) {
 				code.Make(code.OpConstant, 1),
 				code.Make(code.OpSetGlobal, 0),
 				code.Make(code.OpGetGlobal, 0),
-				code.Make(code.OpCall),
+				code.Make(code.OpCall, 0),
 				code.Make(code.OpPop),
 			},
 		},
