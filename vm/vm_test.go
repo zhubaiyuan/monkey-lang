7b2456cb62f39accb1b8435b2be9c919d2faf96a
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-20 16:04:04 +0800

    done - extend tests of implementing functions with arguments and bindings in the vm

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -310,6 +310,38 @@ func TestCallingFunctionsWithArgumentsAndBindings(t *testing.T) {
 		`,
 			expected: 3,
 		},
+		{
+			input: `
+		let sum = fn(a, b) {
+			let c = a + b;
+			c;
+		};
+		sum(1, 2);
+		`,
+			expected: 3,
+		},
+		{
+			input: `
+		let sum = fn(a, b) {
+			let c = a + b;
+			c;
+		};
+		sum(1, 2) + sum(3, 4);`,
+			expected: 10,
+		},
+		{
+			input: `
+		let sum = fn(a, b) {
+			let c = a + b;
+			c;
+		};
+		let outer = fn() {
+			sum(1, 2) + sum(3, 4);
+		};
+		outer();
+		`,
+			expected: 10,
+		},
 	}
 	runVmTests(t, tests)
 }
